fa5a3f31ded567c3a6640fcb5ca8f7f3bcbb8c30
Kusuma J <zenpeaky@gmail.com>
2023-01-06 14:07:18 +0800

    unused import

--- users/views.py
+++ users/views.py
@@ -2,8 +2,6 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
-import urllib.parse
-from django.shortcuts import redirect
 from django.http import JsonResponse
 
 class GoogleLogin(SocialLoginView):
